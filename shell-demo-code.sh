408d45f5af66668f0eb874c1cbfea183fed6ff93
Pronay Sarker <pronaycoding@gmail.com>
2024-05-22 01:05:10 +0600

    Update and rename shell-demo-code.sh to shell-demo-code

--- shell-demo-code.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash
-
-read -p "username: " username #to read name at the same line
-read -sp "password: " password
-echo "username: $username password: $password"
-
-read age
-echo $age
